abd88b76389df0aa94e0fd6f528af665b086a49d
kapil <kapilppatel03@gmail.com>
2023-12-16 18:45:44 +0530

    Expanding functionality with meta information from the page

--- app/services/scrape_data_service.rb
+++ app/services/scrape_data_service.rb
@@ -22,7 +22,18 @@ class ScrapeDataService
     result = {}
     @fields.each do |key, selector|
       result[key] = doc.css(selector).text.strip
+      result[key] = fetch_metadata(doc, selector) if key == 'meta'
     end
     result
+  end
+
+  def fetch_metadata(page, meta)
+    meta_result = {}
+
+    meta.each do |tag|
+      meta_result[tag] = page.at("meta[name='#{tag}']")['content']
+    end
+
+    meta_result
   end
 end
